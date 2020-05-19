ce4a1bb18baa3f9360eec3b876c6f06af18ce76c
Kyra-Hayter <retyahk@gmail.com>
2020-05-19 12:12:44 -0700

    initial commit

---
+++ src/server/index.js
@@ -0,0 +1,43 @@
+//Setup empty JS object to act as endpoint for all routes
+nlpData = {};
+
+var path = require('path')
+
+const dotenv = require('dotenv');
+dotenv.config();
+var aylien = require("aylien_textapi");
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+    });
+console.log(`Your API key is ${process.env.API_KEY}`);
+
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+//Set up instance of an app
+const app = express()
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+//*Middleware
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
+
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('dist/index.html'))
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!')
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
